85caaf96fe7af2ff7320f7712283015b72eb9170
Kenneth P. Vallejos <kenn_vall@yahoo.com>
2012-12-29 19:32:21 +0800

    adds new ipn hanlder that works.

--- application/controllers/paypal.php
+++ application/controllers/paypal.php
@@ -31,14 +31,14 @@ class Paypal extends CI_Controller  {
 		
 		$ch = curl_init();
 		// @mnctodo: open db to get settings for PayPal 
-		curl_setopt($ch, CURLOPT_URL, /*'https://www.sandbox.paypal.com/cgi-bin/webscr'*/ Paypalsettings::env());
+		curl_setopt($ch, CURLOPT_URL, 'https://www.sandbox.paypal.com/cgi-bin/webscr' /*Paypalsettings::env()*/);
 		curl_setopt($ch, CURLOPT_HEADER, 0);
 		curl_setopt($ch, CURLOPT_POST, 1);
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
 		curl_setopt($ch, CURLOPT_POSTFIELDS, $req);
 		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 1);
 		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
-		curl_setopt($ch, CURLOPT_HTTPHEADER, array(/*'Host: www.sandbox.paypal.com'*/ Paypalsettings::envHost()));
+		curl_setopt($ch, CURLOPT_HTTPHEADER, array('Host: www.sandbox.paypal.com'/*Paypalsettings::envHost()*/) );
 		$res = curl_exec($ch);
 		curl_close($ch);
 		
@@ -61,7 +61,11 @@ class Paypal extends CI_Controller  {
 			// check that receiver_email is your Primary PayPal email -- DONE
 			// check that payment_amount/payment_currency are correct
 			// process payment
-			if($_POST['payment_status'] == 'Completed'  && ($this->_isExist_paypal_trnx_id($txn_id) == FALSE)  && $pay_receiver == $receiver_email && $payment_amount == $this->_getTotalAmount($item_name)) {
+			if($_POST['payment_status'] == 'Completed'  
+				&& ($this->_isExist_paypal_trnx_id($txn_id) == FALSE) 
+			 	&& $pay_receiver == $receiver_email 
+			 	&& $payment_amount == $this->_getTotalAmount($item_name)) {
+			 		
 				if($this->_insertOrders($_POST)) {
 					// send email payment confirmation
 					$data = array(
@@ -76,6 +80,88 @@ class Paypal extends CI_Controller  {
 			// log for manual investigation
 		}
 		
+	}
+	
+public function test() {
+
+
+		// STEP 1: Read POST data
+
+		// reading posted data from directly from $_POST causes serialization
+		// issues with array data in POST
+		// reading raw POST data from input stream instead.
+		$raw_post_data = file_get_contents('php://input');
+		$raw_post_array = explode('&', $raw_post_data);
+		$myPost = array();
+		foreach ($raw_post_array as $keyval) {
+			$keyval = explode ('=', $keyval);
+			if (count($keyval) == 2)
+			$myPost[$keyval[0]] = urldecode($keyval[1]);
+		}
+		// read the post from PayPal system and add 'cmd'
+		$req = 'cmd=_notify-validate';
+		if(function_exists('get_magic_quotes_gpc')) {
+			$get_magic_quotes_exists = true;
+		}
+		foreach ($myPost as $key => $value) {
+			if($get_magic_quotes_exists == true && get_magic_quotes_gpc() == 1) {
+				$value = urlencode(stripslashes($value));
+			} else {
+				$value = urlencode($value);
+			}
+			$req .= "&$key=$value";
+		}
+
+
+		// STEP 2: Post IPN data back to paypal to validate
+
+		$ch = curl_init('https://www.sandbox.paypal.com/cgi-bin/webscr');
+		curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
+		curl_setopt($ch, CURLOPT_POST, 1);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $req);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 1);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
+		curl_setopt($ch, CURLOPT_FORBID_REUSE, 1);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, array('Host: www.sandbox.paypal.com'));
+
+		// In wamp like environments that do not come bundled with root authority certificates,
+		// please download 'cacert.pem' from "http://curl.haxx.se/docs/caextract.html" and set the directory path
+		// of the certificate as shown below.
+		// curl_setopt($ch, CURLOPT_CAINFO, dirname(__FILE__) . '/cacert.pem');
+		if( !($res = curl_exec($ch)) ) {
+			// error_log("Got " . curl_error($ch) . " when processing IPN data");
+			curl_close($ch);
+			exit;
+		}
+		curl_close($ch);
+
+
+		// STEP 3: Inspect IPN validation result and act accordingly
+
+		if (strcmp ($res, "VERIFIED") == 0) {
+			// check whether the payment_status is Completed
+			// check that txn_id has not been previously processed
+			// check that receiver_email is your Primary PayPal email
+			// check that payment_amount/payment_currency are correct
+			// process payment
+
+			// assign posted variables to local variables
+			$item_name = $_POST['item_name'];
+			$item_number = $_POST['item_number'];
+			$payment_status = $_POST['payment_status'];
+			$payment_amount = $_POST['mc_gross'];
+			$payment_currency = $_POST['mc_currency'];
+			$txn_id = $_POST['txn_id'];
+			$receiver_email = $_POST['receiver_email'];
+			$payer_email = $_POST['payer_email'];
+			
+			$strQry = sprintf("INSERT INTO testipn SET `status`='%s'", $txn_id . " -- " . $item_name);
+			$this->db->query($strQry);
+			
+		} else if (strcmp ($res, "INVALID") == 0) {
+			// log for manual investigation
+		}
 	}
 	
 	public function thankyou() {
@@ -345,5 +431,5 @@ class Paypal extends CI_Controller  {
 		$this->_mFullName = $arr['advr_fullname'];
 		
 	}
-	
+
 }
