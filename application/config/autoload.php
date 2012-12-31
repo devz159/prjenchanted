6a48919c331c6860ad08efea7432c21d51ba5721
Kenneth P. Vallejos <kenn_vall@yahoo.com>
2013-01-01 03:24:54 +0800

    adds firephp module. this is for debugging purposes. fixes paypal/process [now working]

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -51,7 +51,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('database', 'session', 'xmlrpc');
 */
-$autoload['libraries'] = array('database','form_validation', 'session', 'sessionbrowser', 'sentinel', 'warden', 'paypalsettings', 'googleadsense');
+$autoload['libraries'] = array('database','form_validation', 'session', 'sessionbrowser', 'sentinel', 'warden', 'paypalsettings', 'googleadsense', 'cifb');
 
 
 /*
---
+++ application/libraries/cifb.php
@@ -0,0 +1,22 @@
+<?php 
+
+require_once "/firephp/fb.php";
+	
+class Cifb extends FB {
+	
+	public function __construct() {
+		
+		//echo 'Initialising firephp library<br/ >';
+		
+		// sets some defaults
+		ob_start();
+		FB::setEnabled(FALSE);
+		
+	}
+	
+	public function goEnable() {
+		
+		FB::setEnabled(TRUE);
+		
+	}
+}
